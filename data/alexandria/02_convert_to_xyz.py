65461dcbfa7046e7cf4dc3d8216d820a2f76ee98
Philipp Benner <philipp.benner@bam.de>
2024-12-26 14:23:44 +0100

    implemented parallel processing for alexandria data

--- data/alexandria/02_convert_to_xyz.py
+++ data/alexandria/02_convert_to_xyz.py
@@ -6,13 +6,13 @@ import numpy as np
 import sys
 
 from pathlib import Path
-from tqdm.auto import tqdm
+from filelock import FileLock
+from pqdm.processes import pqdm
 
-from ase import Atoms
-from ase.io import write, read
+from ase.io    import write
 from ase.units import GPa
 
-from pymatgen.core import Structure
+from pymatgen.core   import Structure
 from pymatgen.io.ase import AseAtomsAdaptor
 
 
@@ -20,7 +20,22 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--src_dir", type=str, required=True)
     parser.add_argument("--dst_dir", type=str, required=True)
+    parser.add_argument("--n-jobs" , type=int, default=1)
     return parser.parse_args()
+
+
+def convert_json_item_to_atoms(alex_id, calc_id, ionic_step, w):
+    atoms = AseAtomsAdaptor.get_atoms(Structure.from_dict(w["structure"]))
+    atoms.arrays['forces'] = np.array(w["forces"])
+    atoms.info = {
+        'config_type' : 'Default',
+        "alex_id"     : alex_id,
+        "calc_id"     : calc_id,
+        "ionic_step"  : ionic_step,
+        'energy'      : w["energy"],
+        'stress'      : -np.array(w["stress"]).flatten() * 1e-1 * GPa
+    }
+    return atoms
 
 
 def convert_alexandria_to_atoms(src_path: Path | str):
@@ -36,24 +51,21 @@ def convert_alexandria_to_atoms(src_path: Path | str):
 
     result = dict()
 
-    for alex_id, u in tqdm(data.items(), desc="Converting structures"):
+    for alex_id, u in data.items():
         for calc_id, v in enumerate(u):
             for ionic_step, w in enumerate(v["steps"]):
-                atoms = AseAtomsAdaptor.get_atoms(Structure.from_dict(w["structure"]))
-                atoms.arrays['forces'] = np.array(w["forces"])
-                atoms.info = {
-                    'config_type' : 'Default',
-                    "alex_id"     : alex_id,
-                    "calc_id"     : calc_id,
-                    "ionic_step"  : ionic_step,
-                    'energy'      : w["energy"],
-                    'stress'      : -np.array(w["stress"]).flatten() * 1e-1 * GPa
-                }
 
-                trajectory = result.get(atoms.get_chemical_formula(), [])
-                trajectory.append(atoms)
+                try:
+                    atoms = convert_json_item_to_atoms(alex_id, calc_id, ionic_step, w)
 
-                result[atoms.get_chemical_formula()] = trajectory
+                    trajectory = result.get(atoms.get_chemical_formula(), [])
+                    trajectory.append(atoms)
+
+                    result[atoms.get_chemical_formula()] = trajectory
+
+                except ValueError:
+                    # Catch one entry that seems to have invalid forces array
+                    pass
     
     return result
 
@@ -65,23 +77,40 @@ def export_result(result, dst_dir):
 
     dst_dir.mkdir(exist_ok=True, parents=True)
 
-    for formula, trajectory in tqdm(result.items(), desc="Exporting trajectories"):
-        write(dst_dir / f"{formula}.xyz", trajectory, append=True)
+    for formula, trajectory in result.items():
+
+        filename      = dst_dir / f"{formula}.xyz"
+        filename_lock = dst_dir / f"{formula}.lock"
+
+        with FileLock(filename_lock):
+
+            write(filename, trajectory, append=True)
 
 
-def main(src_dir: Path | str, dst_dir: Path | str):
+def convert_file(src_path, dst_dir):
+    result = convert_alexandria_to_atoms(src_path)
+    export_result(result, dst_dir)
 
-    if Path(dst_dir).exists():
-        print("Output path exists. Exiting...")
+
+def main(src_dir: Path | str, dst_dir: Path | str, n_jobs : int):
+
+    src_dir = Path(src_dir)
+    dst_dir = Path(dst_dir)
+
+    if not src_dir.is_dir():
+        print(f"Source directory '{src_dir}' does not exist or is not a directory.")
         sys.exit(1)
 
-    for src_path in glob.glob(f"{src_dir}/*.json.bz2"):
+    if dst_dir.exists():
+        print(f"Output path '{dst_dir}' exists. Exiting to avoid duplicated entries...")
+        sys.exit(1)
 
-        print(f"Processing {src_path}...")
-        result = convert_alexandria_to_atoms(src_path)
-        export_result(result, dst_dir)
+    files = list(src_dir.rglob("*.json.bz2"))
+    args  = [(str(src_file), str(dst_dir)) for src_file in files]
+
+    pqdm(args, convert_file, argument_type="args", n_jobs=n_jobs)
 
 
 if __name__ == "__main__":
     args = parse_args()
-    main(args.src_dir, args.dst_dir)
+    main(args.src_dir, args.dst_dir, args.n_jobs)
--- data/alexandria/03_join.py
+++ data/alexandria/03_join.py
@@ -1,30 +1,22 @@
 import argparse
-import bz2
 import glob
-import json
-import numpy as np
 import random
 import sys
 
 from pathlib import Path
-from tqdm.auto import tqdm
-
-from ase import Atoms
+from filelock import FileLock
+from pqdm.processes import pqdm
 from ase.io import write, read
-from ase.units import GPa
-
-from pymatgen.core import Structure
-from pymatgen.io.ase import AseAtomsAdaptor
 
 
 class WeightedRandomizer:
     def __init__ (self, weights, seed=42):
         random.seed(seed)
-        self.__max = .0
+        self.__max     = 0.0
         self.__weights = []
         for value, weight in weights.items ():
             self.__max += weight
-            self.__weights.append ( (self.__max, value) )
+            self.__weights.append((self.__max, value))
 
     def __call__(self):
         r = random.random () * self.__max
@@ -32,43 +24,29 @@ class WeightedRandomizer:
             if ceil > r: return value
 
 
+def batch_files(file_list, batch_size):
+    return [file_list[i:i + batch_size] for i in range(0, len(file_list), batch_size)]
+
+
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--src_dir" , type=str, required=True)
-    parser.add_argument("--dst_prefix", type=str, required=True)
+    parser.add_argument("--src_dir"     , type=str  , required=True)
+    parser.add_argument("--dst_prefix"  , type=str  , required=True)
+    parser.add_argument("--train_weight", type=float, default=0.95)
+    parser.add_argument("--valid_weight", type=float, default=0.05)
+    parser.add_argument("--test_weight" , type=float, default=0.00)
+    parser.add_argument("--batch_size"  , type=int  , default=1000)
+    parser.add_argument("--n_jobs"      , type=int  , default=1)
     return parser.parse_args()
 
 
-def main(src_dir: Path | str, dst_prefix: Path | str):
+def process_batch(src_files, filename_train, filename_valid, filename_test, rand):
 
-    filename_train = f"{dst_prefix}_train.xyz"
-    filename_valid = f"{dst_prefix}_valid.xyz"
-    filename_test  = f"{dst_prefix}_test.xyz"
+    result_train = []
+    result_valid = []
+    result_test  = []
 
-    if Path(filename_train).exists():
-        print("Output train file exists. Exiting...")
-        sys.exit(1)
-
-    if Path(filename_valid).exists():
-        print("Output valid file exists. Exiting...")
-        sys.exit(1)
-
-    if Path(filename_test).exists():
-        print("Output test file exists. Exiting...")
-        sys.exit(1)
-
-    # Random assignment to train, valid, or test set
-    rand = WeightedRandomizer({'train': 0.95, 'valid': 0.05, 'test': 0.00})
-
-    # A dictionary used to put all structures with the same composition
-    # to go into the same train, valid, or test set
-    selection_dict = {}
-
-    r_train = []
-    r_valid = []
-    r_test  = []
-
-    for src_file in tqdm(glob.glob(f"{src_dir}/*.xyz"), desc="Joining files"):
+    for src_file in src_files:
 
         selection = rand()
 
@@ -83,13 +61,65 @@ def main(src_dir: Path | str, dst_prefix: Path | str):
             atoms_list = [atoms_list[i1], atoms_list[i2], atoms_list[i3]]
 
         if selection == "train":
-            write(filename_train, atoms_list, append=True)
+            result_train.extend(atoms_list)
         if selection == "valid":
-            write(filename_valid, atoms_list, append=True)
+            result_valid.extend(atoms_list)
         if selection == "test":
-            write(filename_test, atoms_list, append=True)
+            result_test.extend(atoms_list)
+    
+    # Export train data
+    if len(result_train) > 0:
+        with FileLock(filename_train.with_suffix(".lock")):
+            write(filename_train, result_train, append=True)
+
+    # Export train data
+    if len(result_valid) > 0:
+        with FileLock(filename_valid.with_suffix(".lock")):
+            write(filename_valid, result_valid, append=True)
+  
+    # Export test data
+    if len(result_test) > 0:
+        with FileLock(filename_test.with_suffix(".lock")):
+            write(filename_test, result_test, append=True)
+
+
+def main(
+    src_dir     : Path | str,
+    dst_prefix  : Path | str,
+    train_weight: float,
+    valid_weight: float,
+    test_weight : float,
+    batch_size  : int,
+    n_jobs      : int,
+    ):
+
+    filename_train = Path(f"{dst_prefix}_train.xyz")
+    filename_valid = Path(f"{dst_prefix}_valid.xyz")
+    filename_test  = Path(f"{dst_prefix}_test.xyz")
+
+    if filename_train.exists():
+        print("Output train file exists. Exiting...")
+        sys.exit(1)
+
+    if filename_valid.exists():
+        print("Output valid file exists. Exiting...")
+        sys.exit(1)
+
+    if filename_test.exists():
+        print("Output test file exists. Exiting...")
+        sys.exit(1)
+
+    # Random assignment to train, valid, or test set
+    rand = WeightedRandomizer({'train': train_weight, 'valid': valid_weight, 'test': test_weight})
+
+    batches = batch_files(glob.glob(f"{src_dir}/*.xyz"), batch_size)
+    args    = [(batch, filename_train, filename_valid, filename_test, rand) for batch in batches]
+
+    pqdm(args, process_batch, argument_type="args", n_jobs=n_jobs)
 
 
 if __name__ == "__main__":
+
     args = parse_args()
-    main(args.src_dir, args.dst_prefix)
+
+    main(args.src_dir, args.dst_prefix, args.train_weight, args.valid_weight, args.test_weight, args.batch_size, args.n_jobs)
