09f5baefa45199faf583f828de0d582908f7c57f
Philipp Benner <philipp.benner@bam.de>
2024-12-29 20:40:14 +0100

    updated mace test wrapper

--- equitrain/utility_test/wrapper_mace.py
+++ equitrain/utility_test/wrapper_mace.py
@@ -4,12 +4,11 @@ import torch
 
 from tqdm import tqdm
 
+from equitrain.model_wrappers import MaceWrapper
 
-class MaceWrapper(torch.nn.Module):
+class MaceWrapper(MaceWrapper):
 
-    def __init__(self, filename_model = "mace.model", url = "https://github.com/ACEsuit/mace-mp/releases/download/mace_mp_0/2023-12-10-mace-128-L0_epoch-199.model"):
-
-        super().__init__()
+    def __init__(self, args, filename_model = "mace.model", url = "https://github.com/ACEsuit/mace-mp/releases/download/mace_mp_0/2023-12-10-mace-128-L0_epoch-199.model"):
 
         if not os.path.exists(filename_model):
             
@@ -19,14 +18,4 @@ class MaceWrapper(torch.nn.Module):
                 for data in tqdm(response.iter_content(), desc="Downloading MACE"):
                     handle.write(data)
 
-        self.model = torch.load(filename_model)
-
-
-    def forward(self, *args):
-
-        y_pred = self.model(*args, training=self.training)
-
-        if not isinstance(y_pred, dict):
-            y_pred = {'energy': y_pred[0], 'forces': y_pred[1], 'stress': y_pred[2]}
-
-        return y_pred
+        super().__init__(args, torch.load(filename_model))
--- tests/test_train_mace.py
+++ tests/test_train_mace.py
@@ -17,7 +17,7 @@ def test_train_mace():
     args.valid_file      = f'data/valid.h5'
     args.statistics_file = f'data/statistics.json'
     args.output_dir      = 'test_train_mace'
-    args.model           = MaceWrapper()
+    args.model           = MaceWrapper(args)
 
     args.epochs     = 10
     args.batch_size = 64
