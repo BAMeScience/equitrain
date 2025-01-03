b67402fd0108081f391635c020b8d5a7b4128016
Philipp Benner <philipp@zeroprobability.org>
2025-01-03 09:54:16 +0100

    whitespace

--- tests/test_train_mace.py
+++ tests/test_train_mace.py
@@ -15,7 +15,7 @@ def test_train_mace():
 
     args.train_file      = f'data/train.h5'
     args.valid_file      = f'data/valid.h5'
-    args.test_file      = f'data/train.h5'
+    args.test_file       = f'data/train.h5'
     args.statistics_file = f'data/statistics.json'
     args.output_dir      = 'test_train_mace'
     args.model           = MaceWrapper(args)
