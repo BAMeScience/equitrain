7d210b9a275a08abbd14ca008fdc596f52412a06
jpizarro <jose.pizarro-blanco@bam.de>
2025-01-07 11:17:39 +0100

    Formatted tests/ files
    Deleted interactive comments
    
    Added __init__ and conftest modules to tests/
    
    Added missing MaceWrapper import

--- tests/test_predict_mace_atoms.py
+++ tests/test_predict_mace_atoms.py
@@ -1,23 +1,13 @@
-# %%
-import pytest
-
 import ase.io
 
-from pymatgen.core import Structure
-from pymatgen.io.ase import AseAtomsAdaptor
-
-from equitrain import get_args_parser_predict
-from equitrain import predict_atoms
+from equitrain import get_args_parser_predict, predict_atoms
 from equitrain.data.atomic import AtomicNumberTable
-from equitrain.utility     import set_dtype
-
+from equitrain.utility import set_dtype
 from equitrain.utility_test import MaceWrapper
 
-# %%
 
 def test_predict_mace_atoms():
-
-    set_dtype("float64")
+    set_dtype('float64')
 
     r = 4.5
     filename = 'data.xyz'
@@ -26,15 +16,10 @@ def test_predict_mace_atoms():
 
     args.model = MaceWrapper(args)
 
-    atoms_list = ase.io.read(filename, index=":")
+    atoms_list = ase.io.read(filename, index=':')
     z_table = AtomicNumberTable(list(args.model.model.atomic_numbers.numpy()))
 
     energy, force, stress = predict_atoms(args.model, atoms_list, z_table, r)
 
     print(energy)
     print()
-
-
-# %%
-if __name__ == "__main__":
-    test_predict_mace_atoms()
