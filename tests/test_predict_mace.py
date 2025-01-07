a9783e54b8da3ea0615a68403a101562daa5c1e5
jpizarro <jose.pizarro-blanco@bam.de>
2025-01-07 11:17:39 +0100

    Formatted tests/ files
    Deleted interactive comments
    
    Added __init__ and conftest modules to tests/
    
    Added missing MaceWrapper import

--- tests/test_predict_mace.py
+++ tests/test_predict_mace.py
@@ -1,28 +1,17 @@
-# %%
-import pytest
-
-from equitrain import get_args_parser_predict
-from equitrain import predict
-
+from equitrain import get_args_parser_predict, predict
 from equitrain.utility_test import MaceWrapper
 
-# %%
 
 def test_mace_predict():
-
     args = get_args_parser_predict().parse_args()
 
-    args.predict_file    = f'data/valid.h5'
-    args.statistics_file = f'data/statistics.json'
-    args.batch_size      = 5
-    args.model           = MaceWrapper(args)
+    args.predict_file = 'data/valid.h5'
+    args.statistics_file = 'data/statistics.json'
+    args.batch_size = 5
+    args.model = MaceWrapper(args)
 
     energy_pred, forces_pred, stress_pred = predict(args)
 
     print(energy_pred)
     print(forces_pred)
     print(stress_pred)
-
-# %%
-if __name__ == "__main__":
-    test_mace_predict()
--- tests/test_preprocess.py
+++ tests/test_preprocess.py
@@ -1,27 +1,15 @@
-# %%
-
-import pytest
-
-from equitrain import get_args_parser_preprocess
-from equitrain import preprocess
+from equitrain import get_args_parser_preprocess, preprocess
 
 
-# %%
 def test_preprocess():
-
     args = get_args_parser_preprocess().parse_args()
-    
-    args.train_file         = 'data.xyz'
-    args.valid_file         = 'data.xyz'
-    args.output_dir         = 'test_preprocess/'
+
+    args.train_file = 'data.xyz'
+    args.valid_file = 'data.xyz'
+    args.output_dir = 'test_preprocess/'
     args.compute_statistics = True
-    args.atomic_energies    = "average"
-    args.r_max              = 4.5
-    args.verbose            = 1
+    args.atomic_energies = 'average'
+    args.r_max = 4.5
+    args.verbose = 1
 
     preprocess(args)
-
-
-# %%
-if __name__ == "__main__":
-    test_preprocess()
