95b4f648efd086b1346677b915595573cf761f86
jpizarro <jose.pizarro-blanco@bam.de>
2025-01-07 10:12:13 +0100

    Formatting resources/ modules
    Added question for __author__ and __date__ on some modules

--- resources/data/alexandria+mptraj/02_mptraj_to_xyz.py
+++ resources/data/alexandria+mptraj/02_mptraj_to_xyz.py
@@ -1,68 +1,63 @@
 # %% Imports
 import argparse
 import json
-import numpy as np
-import random
-
-from filelock import FileLock
 from pathlib import Path
+
+import numpy as np
 from ase.io import write
 from ase.units import GPa
+from filelock import FileLock
 from pymatgen.core import Structure
 from pymatgen.io.ase import AseAtomsAdaptor
 from tqdm import tqdm
 
-# %%
 
+# %%
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--src_file", type=str, required=True)
-    parser.add_argument("--dst_dir" , type=str, required=True)
-    parser.add_argument("--n-jobs"  , type=int, default=1)
+    parser.add_argument('--src_file', type=str, required=True)
+    parser.add_argument('--dst_dir', type=str, required=True)
+    parser.add_argument('--n-jobs', type=int, default=1)
     return parser.parse_args()
 
+
 # %%
-
 def export_result(result, dst_dir: Path | str):
-
     dst_dir = Path(dst_dir)
 
     dst_dir.mkdir(exist_ok=True, parents=True)
 
     for formula, trajectory in result.items():
-
-        filename      = dst_dir / f"{formula}.xyz"
-        filename_lock = dst_dir / f"{formula}.lock"
+        filename = dst_dir / f'{formula}.xyz'
+        filename_lock = dst_dir / f'{formula}.lock'
 
         # Do not save result if already in Alexandria dataset
         if not filename.exists():
-
             with FileLock(filename_lock):
-
                 write(filename, trajectory)
 
 
 # %%
-
-def main(src_file: Path | str, dst_dir: Path | str, n_jobs : int):
-
+def main(src_file: Path | str, dst_dir: Path | str, n_jobs: int):
     with open(src_file) as f:
         js = json.load(f)
 
     result = dict()
 
     for _, values in tqdm(js.items(), desc='Converting data', total=len(js)):
-
-        formula    = None
+        # formula = None  # ! unused
         trajectory = []
 
         # Extract trajectory
-        for submid, subvalues in values.items():
+        for _, subvalues in values.items():
             atoms = AseAtomsAdaptor.get_atoms(
                 Structure.from_dict(subvalues['structure']),
-                info={'config_type'     : 'Default',
-                    'energy'          : subvalues['uncorrected_total_energy'],
-                    'stress'          : -np.array(subvalues['stress']) * 1e-1 * GPa })
+                info={
+                    'config_type': 'Default',
+                    'energy': subvalues['uncorrected_total_energy'],
+                    'stress': -np.array(subvalues['stress']) * 1e-1 * GPa,
+                },
+            )
             atoms.arrays['forces'] = np.array(subvalues['force'])
 
             trajectory = result.get(atoms.get_chemical_formula(), [])
@@ -74,8 +69,6 @@ def main(src_file: Path | str, dst_dir: Path | str, n_jobs : int):
 
 
 # %%
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     args = parse_args()
     main(args.src_file, args.dst_dir, args.n_jobs)
-
--- resources/data/alexandria/04_xyz_to_hdf5.py
+++ resources/data/alexandria/04_xyz_to_hdf5.py
@@ -1,24 +1,22 @@
 # %%
-from equitrain import get_args_parser_preprocess
-from equitrain import preprocess
+from equitrain import get_args_parser_preprocess, preprocess
 
 
 # %%
 def main():
-
     args = get_args_parser_preprocess().parse_args()
-    
-    args.train_file         = 'alexandria_train.xyz'
-    args.valid_file         = 'alexandria_valid.xyz'
-    args.statistics_file    = 'statistics.json'
-    args.output_dir         = 'alexandria'
+
+    args.train_file = 'alexandria_train.xyz'
+    args.valid_file = 'alexandria_valid.xyz'
+    args.statistics_file = 'statistics.json'
+    args.output_dir = 'alexandria'
     args.compute_statistics = True
-    args.atomic_energies    = "average"
-    args.r_max              = 6.0
+    args.atomic_energies = 'average'
+    args.r_max = 6.0
 
     preprocess(args)
 
 
 # %%
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
