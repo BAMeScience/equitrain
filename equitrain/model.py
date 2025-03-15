909b748a7edbf1382f77bc58ec59d0afbc82a835
Philipp Benner <philipp.benner@bam.de>
2025-03-15 15:14:34 +0100

    cleaned up get_model

--- equitrain/model.py
+++ equitrain/model.py
@@ -2,7 +2,7 @@ import re
 
 import torch
 
-from equitrain.model_wrappers import MaceWrapper, SevennetWrapper
+from equitrain.model_wrappers import AbstractWrapper, MaceWrapper, SevennetWrapper
 
 
 def model_freeze_params(args, model, logger=None):
@@ -34,24 +34,22 @@ def get_model(args, logger=None):
         # TODO: Check if file exists. Through a meaningful error if not
         model = torch.load(args.model, weights_only=False)
 
-    if hasattr(args, 'r_max') and args.r_max is not None:
-        if logger is not None:
-            logger.log(1, f'Overwriting r_max model parameter with r_max={args.r_max}')
-        model.r_max = args.r_max
+    # Apply model wrapper
+    if not isinstance(model, AbstractWrapper):
+        # Set attributes that might be required for the wrapper
+        if not hasattr(args, 'energy_weight'):
+            setattr(args, 'energy_weight', 0.0)
+        if not hasattr(args, 'forces_weight'):
+            setattr(args, 'forces_weight', 0.0)
+        if not hasattr(args, 'stress_weight'):
+            setattr(args, 'stress_weight', 0.0)
 
-    # Set attributes that might be required for the wrapper
-    if not hasattr(args, 'energy_weight'):
-        setattr(args, 'energy_weight', 0.0)
-    if not hasattr(args, 'forces_weight'):
-        setattr(args, 'forces_weight', 0.0)
-    if not hasattr(args, 'stress_weight'):
-        setattr(args, 'stress_weight', 0.0)
+        if args.model_wrapper == 'mace':
+            model = MaceWrapper(args, model)
+        if args.model_wrapper == 'sevennet':
+            model = SevennetWrapper(args, model)
 
-    if args.model_wrapper == 'mace':
-        model = MaceWrapper(args, model)
-    if args.model_wrapper == 'sevennet':
-        model = SevennetWrapper(args, model)
-
+    # Load checkpoint
     if (
         hasattr(args, 'load_checkpoint_model')
         and args.load_checkpoint_model is not None
@@ -62,5 +60,11 @@ def get_model(args, logger=None):
         model.load_state_dict(torch.load(args.load_checkpoint_model, weights_only=True))
 
     model_freeze_params(args, model, logger=logger)
+
+    # Overwrite model parameters
+    if hasattr(args, 'r_max') and args.r_max is not None:
+        if logger is not None:
+            logger.log(1, f'Overwriting r_max model parameter with r_max={args.r_max}')
+        model.r_max = args.r_max
 
     return model
